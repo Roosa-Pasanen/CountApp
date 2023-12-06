acd99d8a0ca2aad85ea2647ca40562acbf3fc1e4
Roosa Pasanen <Roosa Pasanen>
2023-12-06 19:33:32 +0200

    Fullscreen app + näkymät

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Frontpage from "./Frontpage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="sidebar" style={{ width: "25%" }}>
+        <div className="sidebar-item">
+          <a href="/" className="button">
+            Home
+          </a>
+        </div>
+        <div className="sidebar-item">
+          <a href="/statistics" className="button">
+            Statistics
+          </a>
+        </div>
+        <div className=" sidebar-item">
+          <a href="/about" className="button">
+            About
+          </a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div style={{ marginLeft: "25%", width: "75%" }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Frontpage />} />
+            <Route path="/about" element={"bloop"} />
+            <Route path="/statistics" element={"oops"} />
+          </Routes>
+        </BrowserRouter>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Frontpage.jsx
@@ -0,0 +1,9 @@
+export default function Frontpage() {
+  return (
+    <div>
+      moimoimoimiommoimoimoimoimiom
+      <p>goiergoijrig</p>
+      <p>rgaiorgijaijgoajgroijaoigjariogjoierajgioajgoia</p>
+    </div>
+  );
+}
