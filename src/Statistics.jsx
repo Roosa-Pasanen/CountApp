7df48e8863594f305a3cedde80332fcaf5ba370a
Roosa Pasanen <Roosa Pasanen>
2023-12-06 19:55:10 +0200

    Statistics.jsx: Add text

---
+++ src/Statistics.jsx
@@ -0,0 +1,17 @@
+export default function Statistics() {
+  return (
+    <div>
+      <p>Instructions: Currently not needed (page has no functionality)</p>
+      <p>Used external libraries: Currently none</p>
+      <p>Usage of AI: none</p>
+      <p>Total coding time: 2h</p>
+      <ul>
+        Nykyiset ongelmat:
+        <li>Appi fullscreeniin (vie enemmän kuin 340px)</li>
+        <li>Tietokantaan yhteenotto</li>
+        <li>Tein vahingossa tämän statistiikkojen paikalle :'D</li>
+      </ul>
+      <b>Made by Roosa Pasanen</b>
+    </div>
+  );
+}
