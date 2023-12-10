971c6466d748ffeda7cd9f05b88fbd3e13458080
Roosa Pasanen <Roosa Pasanen>
2023-12-10 17:28:34 +0200

    add edit button

--- src/Task.jsx
+++ src/Task.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 export default function Task(props) {
   const [tagState, setTagState] = useState(0);
+  const [editState, setEditState] = useState(false);
 
   useEffect(() => {
     let tagArray = [];
@@ -12,10 +13,26 @@ export default function Task(props) {
     setTagState(<ul>Tags: {tagArray} </ul>);
   }, [props.tags]);
 
-  return (
-    <div style={{ backgroundColor: "antiquewhite" }}>
-      <b>{props.name}</b>
-      <div>{tagState}</div>
-    </div>
-  );
+  useEffect(() => {}, [setEditState]);
+
+  function editable() {
+    if (!editState) {
+      return (
+        <div style={{ backgroundColor: "antiquewhite" }}>
+          <b>{props.name}</b>
+          <button onClick={() => setEditState(true)}> Edit </button>
+          <div>{tagState}</div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p>Bop</p>
+          <button onClick={() => setEditState(false)}> oops </button>
+        </div>
+      );
+    }
+  }
+
+  return editable();
 }
