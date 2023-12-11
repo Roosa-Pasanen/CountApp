059b0fe87cbd80e5377d886b02336095749791d7
Roosa Pasanen <Roosa Pasanen>
2023-12-11 12:22:12 +0200

    TaskArray.jsx: Move contents from Frontpage.jsx

--- src/Frontpage.jsx
+++ src/Frontpage.jsx
@@ -1,43 +1,12 @@
 import { useEffect, useState } from "react";
-import Task from "./task.jsx";
+import TaskArray from "./TaskArray.jsx";
+import EditTask from "./EditTask.jsx";
 
 export default function Frontpage() {
-  const [displayState, setDisplayState] = useState(null);
-  //const [didMount, setDidMount] = useState(false);
-
-  useEffect(() => {
-    async function dataFetch() {
-      try {
-        const info = await fetch("http://localhost:3010/tasks");
-        const infoparse = await info.json();
-        setDisplayState(infoparse);
-        console.log(infoparse);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    dataFetch();
-  }, []);
-
-  function createTaskList() {
-    try {
-      const info = displayState;
-      if (info == null) {
-        return <p>Fetching database information, please wait a moment</p>;
-      } else {
-        let taskArray = [];
-        for (let i = 0; i < info.length; i++) {
-          console.log(info[i].name + ": " + info[i].tags);
-          taskArray.push(
-            <Task key={info[i].id} name={info[i].name} tags={info[i].tags} />
-          );
-        }
-        return taskArray;
-      }
-    } catch (err) {
-      return <p>Something went wrong while fetching from database</p>;
-    }
-  }
-
-  return createTaskList();
+  return (
+    <>
+      <EditTask name={"hello"} />
+      <TaskArray />
+    </>
+  );
 }
---
+++ src/TaskArray.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import Task from "./task.jsx";
+
+export default function TaskArray() {
+  const [displayState, setDisplayState] = useState(null);
+
+  useEffect(() => {
+    async function dataFetch() {
+      try {
+        const info = await fetch("http://localhost:3010/tasks");
+        const infoparse = await info.json();
+        setDisplayState(infoparse);
+        console.log(infoparse);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    dataFetch();
+  }, []);
+
+  function createTaskList() {
+    try {
+      const info = displayState;
+      if (info == null) {
+        return <p>Fetching database information, please wait a moment</p>;
+      } else {
+        let taskArray = [];
+        for (let i = 0; i < info.length; i++) {
+          console.log(info[i].name + ": " + info[i].tags);
+          taskArray.push(
+            <Task key={info[i].id} name={info[i].name} tags={info[i].tags} />
+          );
+        }
+        return taskArray;
+      }
+    } catch (err) {
+      return <p>Something went wrong while fetching from database</p>;
+    }
+  }
+
+  return createTaskList();
+}
