013eac55f8895327ab078a6a4001fffa4e3383ba
Roosa Pasanen <Roosa Pasanen>
2023-12-11 12:22:35 +0200

    EditTask.jsx: Edit default name

--- src/EditTask.jsx
+++ src/EditTask.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 export default function EditTask(props) {
-  const [nameState, setNameState] = useState("Default");
+  const [nameState, setNameState] = useState(props.name);
   return (
     <>
       <input value={nameState} onChange={(e) => setNameState(e.target.value)} />
