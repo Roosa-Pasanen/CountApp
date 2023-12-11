16929400e3c18fead60d28231cd1bec7a7943296
Roosa Pasanen <Roosa Pasanen>
2023-12-11 11:47:17 +0200

    EditTask.jsx: Add input field

--- src/EditTask.jsx
+++ src/EditTask.jsx
@@ -1,3 +1,10 @@
 import { useState, useEffect } from "react";
 
-export default function EditTask(props) {}
+export default function EditTask(props) {
+  const [nameState, setNameState] = useState("Default");
+  return (
+    <>
+      <input value={nameState} onChange={(e) => setNameState(e.target.value)} />
+    </>
+  );
+}
