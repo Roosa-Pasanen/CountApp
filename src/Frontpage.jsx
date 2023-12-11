c0f7f1aaad4084fda7bcc407f0d7e800180cb154
Roosa Pasanen <Roosa Pasanen>
2023-12-11 12:16:23 +0200

    Frontpage.jsx:change component from class based to function based

--- src/Frontpage.jsx
+++ src/Frontpage.jsx
@@ -1,44 +1,43 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Task from "./task.jsx";
 
-export default class Frontpage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.createTaskList = this.createTaskList.bind(this);
-    this.state = {
-      display: "",
-    };
-  }
+export default function Frontpage() {
+  const [displayState, setDisplayState] = useState(null);
+  //const [didMount, setDidMount] = useState(false);
 
-  async componentDidMount() {
+  useEffect(() => {
+    async function dataFetch() {
+      try {
+        const info = await fetch("http://localhost:3010/tasks");
+        const infoparse = await info.json();
+        setDisplayState(infoparse);
+        console.log(infoparse);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    dataFetch();
+  }, []);
+
+  function createTaskList() {
     try {
-      const info = await fetch("http://localhost:3010/tasks");
-      const infoparse = await info.json();
-      this.setState({
-        display: infoparse,
-      });
-      console.log(infoparse);
+      const info = displayState;
+      if (info == null) {
+        return <p>Fetching database information, please wait a moment</p>;
+      } else {
+        let taskArray = [];
+        for (let i = 0; i < info.length; i++) {
+          console.log(info[i].name + ": " + info[i].tags);
+          taskArray.push(
+            <Task key={info[i].id} name={info[i].name} tags={info[i].tags} />
+          );
+        }
+        return taskArray;
+      }
     } catch (err) {
-      console.log(err);
+      return <p>Something went wrong while fetching from database</p>;
     }
   }
 
-  /*componentDidUpdate() {
-  }*/
-
-  createTaskList() {
-    const info = this.state.display;
-    let taskArray = [];
-    for (let i = 0; i < info.length; i++) {
-      console.log(info[i].name + ": " + info[i].tags);
-      taskArray.push(
-        <Task key={info[i].id} name={info[i].name} tags={info[i].tags} />
-      );
-    }
-    return taskArray;
-  }
-
-  render() {
-    return this.createTaskList();
-  }
+  return createTaskList();
 }
